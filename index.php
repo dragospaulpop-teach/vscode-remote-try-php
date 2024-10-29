a49657edb2a25e7e3d1c05a2d44ec3db547640a0
Dragos-Paul Pop <147153001+dragospaulpop-teach@users.noreply.github.com>
2024-10-29 14:55:51 +0000

    add date

--- index.php
+++ index.php
@@ -1,13 +1,17 @@
 <html>
-	<head>
-		<title>Visual Studio Code Remote :: PHP</title>
-	</head>
-	<body>
-		<?php
-                    
-        // phpinfo();
-        echo "Hello world";
 
-		?>
-	</body>
+<head>
+	<title>Visual Studio Code Remote :: PHP</title>
+</head>
+
+<body>
+	<?php
+
+	// phpinfo();
+	echo "Hello world";
+
+	echo date("Y F");
+	?>
+</body>
+
 </html>
